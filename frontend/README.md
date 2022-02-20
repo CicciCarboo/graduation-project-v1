900648fefa74adc520534e7dca1c2117d194fea0
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-02-20 13:32:40 +0100

    Frontend startup

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+<h1>Välkommen till LetsGO!</h1>
+      <h2>din digitala kom-ihåg-lista.</h2>
+      <h3>Ny startar vi dagen. Let's GO!</h3>
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Välkommen till LetsGO!</h1>
+      <h2>din digitala kom-ihåg-lista.</h2>
+      <h3>Ny startar vi dagen. Let's GO!</h3>
+    </div>
+  );
+}
+
+export default App;
