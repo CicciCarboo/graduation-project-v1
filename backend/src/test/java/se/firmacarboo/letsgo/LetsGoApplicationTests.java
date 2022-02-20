83105106f5aed530db2b6b316fbd71c7c3a61e3c
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-02-20 11:48:09 +0100

    Backend setup

---
+++ backend/src/test/java/se/firmacarboo/letsgo/LetsGoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.firmacarboo.letsgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
