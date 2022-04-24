eb83e01bc482c2318f77158a80741eddca5d7038
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-04-24 17:44:39 +0200

    TodoList rendering TodoItems
    working with fetched data, but problem with loading.

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -5,7 +5,7 @@ const TodoItem = (props) => {
         <div className="todo-item__container">
             <div className="todo-item__wrapper">
                 <label className="list-group-item d-flex justify-content-between align-items-center">
-                    <input className="form-check-input me-1" type="checkbox" value={props.todo}/>
+                    <input className="form-check-input me-1" type="checkbox" value={props.task}/>
                         <div className="star__containter">
                             <div className="star__wrapper">***STAR!***
                                 <img className="star" alt="Star acting as check-box, to be checked when task is completed."/>
@@ -13,12 +13,12 @@ const TodoItem = (props) => {
                         </div>
                         <div className="todo__containter">
                             <div className="todo__wrapper">
-                                <p className="todo__paragraph m-0">{props.todo.todo}</p>
+                                <p className="todo__paragraph m-0">{props.task.name}</p>
                             </div>
                         </div>
                         <div className="image__containter">
                             <div className="image__wrapper">
-                                <img src={props.todo.imageUrl} className="img-thumbnail" alt={props.todo.todo}/>
+                                <img src={props.task.imageUrl} className="img-thumbnail" alt={props.task.imageAltText}/>
                             </div>
                         </div>
                 </label>
--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,7 +1,5 @@
-import React, {useEffect} from 'react';
 import TodoItem from "./TodoItem";
 import useFetch from "./useFetch";
-import axios from "axios";
 
 
 const TodoList = () => {
@@ -12,22 +10,32 @@ const TodoList = () => {
     //from GeeksForGeeks
 //    This is the original useFetch hook from GeeksForGeeks, but it doesn't work, because the variable/object "data" can not be recognised in the JSX when called as such: console.log(data).
 //    The reason beeing: GeeksForGeeks called the field by the wrong name, they should have called it by "dataInfo".
-    const { data: dataInfo, loading, error, refetch } = useFetch(
+    const { data, loading, error, refetch } = useFetch(
         "http://localhost:8080/api/v1/images");
 
 
     console.log("loading: ", loading)
     console.log("error: ", error)
     /*{console.log("refetch: ", refetch)} Triggers too many rerenderings.*/
-    console.log("data: ", dataInfo)
+    console.log("data: ", data)
     // console.log("data: ", dataInfo[2].name) Does not work during loading, Can only load after data state is set the first time.
+
+    const taskArray = data;
 
 
     return (
         <div className="todo-list__container list-group">
-            <ul className="todo-ul p-0">
-                {todoArray.map((todo,index)=><TodoItem key={index} todo={todo} index={index}/>)}
-            </ul>
+            {loading ? <p>Loading....</p> : (
+                <ul>
+                    Ready to display! try useFetch in an useEffect, otherwise problems with loading!
+                    {/*{taskArray.map((task, index)=><TodoItem key={index} task={task}/>)}*/}
+
+                </ul>
+            )
+            }
+            {/*<ul className="todo-ul p-0">                */}
+            {/*    {todoArray.map((todo,index)=><TodoItem key={index} todo={todo} index={index}/>)}*/}
+            {/*</ul>*/}
 
         </div>
     );
