c4e2d51551467c6e0bd2526fdc9f71b4e5cfeef0
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-09 15:02:43 +0200

    correcting typ-o in className

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -7,17 +7,17 @@ const TodoItem = (props) => {
             <div className="todo-item__wrapper">
                 <label className="list-group-item d-flex justify-content-between align-items-center">
                     <input className="form-check-input me-1" type="checkbox" value={props.task.usageType}/>
-                        <div className="star__containter">
+                        <div className="star__container">
                             <div className="star__wrapper">***STAR!***
                                 <img className="star" alt="Star acting as check-box, to be checked when task is completed."/>
                             </div>
                         </div>
-                        <div className="todo__containter">
+                        <div className="todo__container">
                             <div className="todo__wrapper">
                                 <p className="todo__paragraph m-0">{props.task.message}</p>
                             </div>
                         </div>
-                        <div className="image__containter">
+                        <div className="image__container">
                             <div className="image__wrapper">
                                 <img src={props.task.imageUrl} className="img-thumbnail" alt={props.task.imageAltText}/>
                             </div>
