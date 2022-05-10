3fd209354d8c8d0ee1ac847c29229ed0180a6d63
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-10 11:56:41 +0200

    Star added, color variable added

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -2,6 +2,11 @@ import React from 'react';
 import "./../styles/TodoItem.css";
 
 const TodoItem = (props) => {
+
+    // let starColor = "warning";
+    let starColor = "danger";
+
+
     console.log("From TodoItem.js: props.tasks: ",props.task);
     return (
         <div className="todo-item__container">
@@ -9,8 +14,13 @@ const TodoItem = (props) => {
                 <label className="list-group-item d-flex justify-content-between align-items-center">
                     <input className="form-check-input me-1" type="checkbox" value={props.task.usageType}/>
                         <div className="star__container">
-                            <div className="star__wrapper">***STAR!***
-                                <img className="star" alt="Star acting as check-box, to be checked when task is completed."/>
+                            <div className="star__wrapper">
+                                {/*<i className="bi bi-star-fill"></i>*/}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     className= {`star bi bi-star-fill text-${starColor}`} viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                </svg>
                             </div>
                         </div>
                         <div className="todo__container">
