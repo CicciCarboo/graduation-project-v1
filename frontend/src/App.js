d224e5e90b3bd0003e9b06e992068948c28f2181
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-18 16:40:34 +0200

    Intent to make star responsive, work in progress!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,12 @@ function App() {
       <h1>Välkommen till LetsGO!</h1>
       <h2>din digitala kom-ihåg-lista.</h2>
       <h3>Ny startar vi dagen. Let's GO!</h3>
+        {/*<p>vidareutvecklingförslag från testperson 1:*/}
+        {/*   Bild för frukost, toabesök, duscha efter träningen</p>*/}
+        {/*<br/>*/}
+        {/*<p>vidareutvecklingförslag från testperson 2:*/}
+        {/*    Pusselbitar som användaren själv kan dra och fylla på listan med.*/}
+        {/*    Checklistor för olika tillfällen: Morgon, träning, kväll, skönhetsrutin.</p>*/}
         <TodoList/>
     </div>
   );
