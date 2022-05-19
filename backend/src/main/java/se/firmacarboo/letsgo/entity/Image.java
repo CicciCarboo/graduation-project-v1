c42d3690d498caa06af79452054337c65abfff44
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-19 13:22:13 +0200

    Clean up.

--- backend/src/main/java/se/firmacarboo/letsgo/entity/Image.java
+++ backend/src/main/java/se/firmacarboo/letsgo/entity/Image.java
@@ -1,12 +1,8 @@
 package se.firmacarboo.letsgo.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
--- backend/src/main/java/se/firmacarboo/letsgo/repository/ImageRepository.java
+++ backend/src/main/java/se/firmacarboo/letsgo/repository/ImageRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface ImageRepository  extends MongoRepository<Image, String> {
 
-    //TODO you should disable the use of arbitrary JavaScript by setting javascriptEnabled:false in the config file. / https://www.infoq.com/articles/Starting-With-MongoDB/
     Optional<Image> findImageByImageUrl(String imageUrl);
 }
