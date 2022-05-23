55a30d8706990a03dc1f67318e8ee7e304c464fd
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-23 15:32:55 +0200

    Removed unused imports.

--- backend/src/main/java/se/firmacarboo/letsgo/LetsGoApplication.java
+++ backend/src/main/java/se/firmacarboo/letsgo/LetsGoApplication.java
@@ -1,19 +1,6 @@
 package se.firmacarboo.letsgo;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import se.firmacarboo.letsgo.entity.Image;
-import se.firmacarboo.letsgo.entity.UsageType;
-import se.firmacarboo.letsgo.repository.ImageRepository;
-
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class LetsGoApplication {
