621d17a4b658995df8dfe1879ed77a1e15306e92
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-05-31 13:12:05 +0200

    reset-checkbox replaced with button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,16 @@ import {useState} from "react";
 function App() {
 
     // state to change checked
-    const [resetIsChecked, setResetIsChecked] = useState(false);
+    const [resetIsClicked, setResetIsClicked] = useState(false);
 
-    const handleResetChange = () => {
-
-        if(resetIsChecked){
-            setResetIsChecked(false);
-        }else {
-            setResetIsChecked(true);
+    const handleResetClick = () => {
+        if (resetIsClicked) {
+            setResetIsClicked(false);
+        } else {
+            setResetIsClicked(true);
         }
-        // console.log("Reset-checkbox status: ", resetIsChecked);
     }
+    // console.log("After handleResetChange, resetIsClicked: ", resetIsClicked);
 
     return (
         <div className="App container bg-warning bg-opacity-10">
@@ -25,17 +24,19 @@ function App() {
                     <h1 className="fw-semibold fs-4">Ny startar vi dagen.</h1>
                     <h2 className="fw-bold fs-2">Let's GO!</h2>
                 </div>
-                <input id="reset-todo-items_checkbox" type="checkbox" checked={resetIsChecked} onChange={handleResetChange}/>
-                <label className="d-flex justify-content-around" htmlFor="reset-todo-items_checkbox">Återställ listan</label>
             </div>
-            <TodoList resetIsChecked={resetIsChecked} setResetIsChecked={setResetIsChecked}/>
-            <div className="source_div pt-4">
+            <TodoList resetIsChecked={resetIsClicked} setResetIsChecked={setResetIsClicked}/>
+            <div className="reset-button_wrapper d-flex flex-start p-4">
+            <button type="button" className="btn btn-success" onClick={handleResetClick}>Återställ listan
+            </button>
+            </div>
+            <div className="source_div pt-2">
                 <p className="m-0">Bildkällor: </p>
                 <p className="m-0">Papunets bildbank, <a href="https://papunet.net/">www.papunet.net</a>, </p>
                 <p className="m-0"> Sergio Palao / ARASAAC, Källa:
                     <a href="http://www.catedu.es/arasaac/">ARASAAC</a>, </p>
                 <p className="m-0"> Sclera, Källa: <a
-                        href="http://sclera.be/">Sclera</a>
+                    href="http://sclera.be/">Sclera</a>
                     , hämtade ca 2016</p>
             </div>
         </div>
---
+++ frontend/src/components/MyDay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyDay = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyDay;
